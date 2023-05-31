13b5348b428f9d2535ee1800f6ced4c819737973
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-05-31 11:39:00 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { init, checkForEntry, compareGuess, reset } from './function.js';
 import './App.css'
+
+function handleKeyDown(event) {
+  const input = event.target;
+  const value = parseInt(input.value + event.key, 10);
+  const isNumberOutOfRange = value < input.min || value > input.max;
+
+  if (isNaN(value) || isNumberOutOfRange) {
+    event.preventDefault();
+  }
+}
 
 
 function App() {
@@ -10,15 +20,24 @@ function App() {
 
   return (
     <>
-      <h1>Adivinhe o Numero</h1>
-      <p>Selecionei um número entre 1 e 999.</p>
-      <label for='palpite'>Seu palpite aqui: </label>
-      <input id='palpite' type='number' min='1' max='999' onKeyPress={checkForEntry} autofocus />
-      <button id='button' onClick={compareGuess}>Check</button>
-      <p></p>
-      <div id='result'></div>
-      <p></p>
-      <button id='reset' onClick={reset}>Resetar</button>
+      <div className="center">
+        <h1>Adivinhe o Numero</h1>
+        <div className="bot">
+          <img src="https://cdn.dribbble.com/users/37530/screenshots/2937858/media/658636eccf753ce27bb6c83ba136928a.gif" alt="Robo" />
+          <p className='fala-bot'>Olá eu selecionei um número entre 1 e 999 e quero que você adivinhe qual escolhi.</p>
+        </div>
+        <div className="palpite">
+          <label for='palpite'>Seu palpite aqui: </label>
+          <input id='palpite' type='number' min="1" max="999" onKeyPress={checkForEntry} onKeyDown={handleKeyDown} autofocus />
+          <button id='button' onClick={compareGuess}>Check</button>
+        </div>
+        <p></p>
+        <div className='result-palpite' id='result'></div>
+        <p></p>
+        <div className="reset">
+          <button id='reset' onClick={reset}>Resetar</button>
+        </div>
+      </div>
     </>
   )
 }
